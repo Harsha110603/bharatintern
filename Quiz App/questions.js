cdd86f3c99f7006c9a6e5215791ac3d89c5a37a7
Sai Harsha Vardhan Reddy N <140871244+Harsha110603@users.noreply.github.com>
2023-12-10 20:30:36 +0530

    Add files via upload

---
+++ Quiz App/questions.js
@@ -0,0 +1,47 @@
+const questions = [
+  {
+    question: 'What is the capital of France?',
+    answers: [
+      { text: 'Paris', correct: true },
+      { text: 'Berlin', correct: false },
+      { text: 'Madrid', correct: false },
+      { text: 'Rome', correct: false }
+    ]
+  },
+  {
+    question: 'Which planet is known as the Red Planet?',
+    answers: [
+      { text: 'Mars', correct: true },
+      { text: 'Jupiter', correct: false },
+      { text: 'Venus', correct: false },
+      { text: 'Saturn', correct: false }
+    ]
+  },
+  {
+    question: 'What is the largest mammal?',
+    answers: [
+      { text: 'Elephant', correct: false },
+      { text: 'Blue Whale', correct: true },
+      { text: 'Giraffe', correct: false },
+      { text: 'Hippopotamus', correct: false }
+    ]
+  },
+  {
+    question: 'Who wrote "Romeo and Juliet"?',
+    answers: [
+      { text: 'Charles Dickens', correct: false },
+      { text: 'Jane Austen', correct: false },
+      { text: 'William Shakespeare', correct: true },
+      { text: 'Mark Twain', correct: false }
+    ]
+  },
+  {
+    question: 'What is the powerhouse of the cell?',
+    answers: [
+      { text: 'Mitochondria', correct: true },
+      { text: 'Nucleus', correct: false },
+      { text: 'Endoplasmic Reticulum', correct: false },
+      { text: 'Ribosome', correct: false }
+    ]
+  }
+];
